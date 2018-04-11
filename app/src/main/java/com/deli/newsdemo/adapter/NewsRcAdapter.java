1d2e5e85ca8a1cd8a6729e7faad041c8964361e8
qiudeli <deliqiu@sf-express.com>
2018-04-11 11:18:05 +0800

    【优化】修改glide版本，添加news 的过度loading

--- app/src/main/java/com/deli/newsdemo/adapter/NewsRcAdapter.java
+++ app/src/main/java/com/deli/newsdemo/adapter/NewsRcAdapter.java
@@ -6,9 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.deli.newsdemo.R;
 import com.deli.newsdemo.entity.NewsHeadlineEntity;
 
@@ -37,12 +41,24 @@ public class NewsRcAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         if (holder instanceof NewsHolder) {
-            NewsHolder newsHolder = (NewsHolder) holder;
+            final NewsHolder newsHolder = (NewsHolder) holder;
             NewsHeadlineEntity.T1348647853363Bean item = (NewsHeadlineEntity.T1348647853363Bean) mData.get(position);
             newsHolder.newsTitle.setText(item.getTitle());
             newsHolder.newsTips.setText(item.getSource());
             Glide.with(mContext)
                     .load(item.getImgsrc())
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            newsHolder.mProgressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
                     .into(newsHolder.mImageView);
         }
     }
@@ -52,16 +68,19 @@ public class NewsRcAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         return mData.size();
     }
 
+
     class NewsHolder extends RecyclerView.ViewHolder {
         TextView newsTitle;
         TextView newsTips;
         ImageView mImageView;
+        ProgressBar mProgressBar;
 
         public NewsHolder(View itemView) {
             super(itemView);
             newsTitle = itemView.findViewById(R.id.news_title);
             newsTips = itemView.findViewById(R.id.tips);
             mImageView = itemView.findViewById(R.id.news_img);
+            mProgressBar = itemView.findViewById(R.id.progress);
         }
     }
 
