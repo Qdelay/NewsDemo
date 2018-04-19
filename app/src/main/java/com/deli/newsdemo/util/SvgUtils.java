cc24a02bcdaedcc82df155e0158ed205a03760fe
qiudeli <deliqiu@sf-express.com>
2018-04-19 17:34:51 +0800

    【优化】svgUtil添加版本判断 & 加入unicode解码

--- app/src/main/java/com/deli/newsdemo/util/SvgUtils.java
+++ app/src/main/java/com/deli/newsdemo/util/SvgUtils.java
@@ -2,21 +2,24 @@ package com.deli.newsdemo.util;
 
 import android.content.Context;
 import android.graphics.drawable.VectorDrawable;
+import android.os.Build;
 import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
 import android.widget.ImageView;
 
 /**
  * Created by qiudeli on 2018/1/21.
- *
+ * <p>
  * SVG帮助类
  */
 
 public final class SvgUtils {
 
     public static void changeSvgColor(Context context, ImageView imageView, @ColorRes int ColorRes) {
-        VectorDrawable drawable = (VectorDrawable) (imageView.getDrawable());
-        drawable.mutate();
-        drawable.setTint(ContextCompat.getColor(context, ColorRes));
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            VectorDrawable drawable = (VectorDrawable) (imageView.getDrawable());
+            drawable.mutate();
+            drawable.setTint(ContextCompat.getColor(context, ColorRes));
+        }
     }
 }
